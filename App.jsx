7443103f4027dfbaa53f7f1f1512ec0628b89b20
AshishG-21 <ashish.g_bca24-28@macfast.ac.in>
2026-04-08 16:30:04 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,56 +1,28 @@
-from fastapi import FastAPI, HTTPException
+import react,{ useState,useEffect } from 'react';
 
-app = FastAPI()
+function App() {
+  const [name, setName] = useState("");
+  const [slotName, setRoomName] = useState("");
+const [employees, setEmployees] = useState([]);
+const [slots,setRooms] = useState([]);
 
-employees=[]
-rooms=[]
-assignments=[]
-
-
-@app.post("/employees")
-def add_employees(name:str):
-    emp= {"id":len(employees)+1,"name":name}
-    employees.append(emp)
-    return emp
-
-@app.get("/get_employees")
-def get_employees():
-    return employees
-
-@app.post("/rooms")
-def add_rooms(name:str):
-    room={"id":len(rooms)+1, "room_name":name}
-    rooms.append(room)
-    return room
-
-@app.get("/get_rooms")
-def get_rooms():
-    return rooms
-
-@app.post("/assign")
-def assign(employee_id:int,room_id:int):
-    emp=next((e for e in employees if e["id"]==employee_id),None)
-    if not emp:
-        raise HTTPException (status_code=404,detail="Employee not found")
-
-    room=next((r for r in rooms if r["id"]==room_id),None)
-    if not room:
-        raise HTTPException(status_code=404,detail="Room not found")
-    
-    for a in assignments:
-        if a["room_id"]==room_id:
-            raise HTTPException(status_code=404,detail="Already assigned")
-    
-    assignment={"employee_id":employee_id, "room_id":room_id}
-    assignments.append(assignment)
-    return assignment
-
-@app.get("/assignments")
-def get_assignments():
-    return assignments
-    
+const API="http://127.0.0.1:8000";
 
 
+useEffect(()=>{
+  fetch(API+ "/employees").then(res=>res.JSON).then("setEmployees");
+  fetch(API + "/rooms").then(res=>res.JSON).then("setRooms");
+},[]);
 
+const addEmployee= async()=>{
+  await fetch(API + "/employees?name=" + name,{method:"POST"});
+  window.location.reload();
+};
+  const addRoom=async()=>{
+  await fetch(API + "/rooms?name=" + roomName,{method:"POST"});
+  window.location.reload();
+};
+}
 
-
+export default App;
+ 
