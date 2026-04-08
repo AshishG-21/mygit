d1e26f349b89c8d5c65a7ccaa5625cc5429110b2
AshishG-21 <ashish.g_bca24-28@macfast.ac.in>
2026-04-08 16:29:05 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,121 +1,56 @@
-import { useState } from 'react'
+from fastapi import FastAPI, HTTPException
 
-function App() {
-  const [name, setName] = useState("")
-  const [slotName, setSlotName] = useState("")
-const [employees, setEmployees] = useState([])
-const [slots,setSlots] = useState([])
+app = FastAPI()
+
+employees=[]
+rooms=[]
+assignments=[]
 
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+@app.post("/employees")
+def add_employees(name:str):
+    emp= {"id":len(employees)+1,"name":name}
+    employees.append(emp)
+    return emp
 
-      <div className="ticks"></div>
+@app.get("/get_employees")
+def get_employees():
+    return employees
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+@app.post("/rooms")
+def add_rooms(name:str):
+    room={"id":len(rooms)+1, "room_name":name}
+    rooms.append(room)
+    return room
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
+@app.get("/get_rooms")
+def get_rooms():
+    return rooms
 
-export default App
+@app.post("/assign")
+def assign(employee_id:int,room_id:int):
+    emp=next((e for e in employees if e["id"]==employee_id),None)
+    if not emp:
+        raise HTTPException (status_code=404,detail="Employee not found")
+
+    room=next((r for r in rooms if r["id"]==room_id),None)
+    if not room:
+        raise HTTPException(status_code=404,detail="Room not found")
+    
+    for a in assignments:
+        if a["room_id"]==room_id:
+            raise HTTPException(status_code=404,detail="Already assigned")
+    
+    assignment={"employee_id":employee_id, "room_id":room_id}
+    assignments.append(assignment)
+    return assignment
+
+@app.get("/assignments")
+def get_assignments():
+    return assignments
+    
+
+
+
+
+
