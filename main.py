5188fe0367303178c472e49a4bfc6df81f44c154
AshishG-21 <ashish.g_bca24-28@macfast.ac.in>
2026-04-08 16:27:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 employees=[]
-slots=[]
+rooms=[]
 assignments=[]
 
-@app.get("/root")
-def home():
-    return {"message":"Hello World"}
 
 @app.post("/employees")
 def add_employees(name:str):
-    emp= {"id":len(employees)+1,
-          "name":name}
+    emp= {"id":len(employees)+1,"name":name}
     employees.append(emp)
     return emp
 
@@ -21,35 +17,33 @@ def add_employees(name:str):
 def get_employees():
     return employees
 
-@app.post("/slots")
-def add_slots(name:str):
-    sl={"id":len(slots)+1,
-       "slot_name":name}
-    slots.append(sl)
-    return sl
+@app.post("/rooms")
+def add_rooms(name:str):
+    room={"id":len(rooms)+1, "room_name":name}
+    rooms.append(room)
+    return room
 
-@app.get("/get_slots")
-def get_slots():
-    return slots
+@app.get("/get_rooms")
+def get_rooms():
+    return rooms
 
 @app.post("/assign")
-def assign(employee_id:int,slot_id:int):
+def assign(employee_id:int,room_id:int):
     emp=next((e for e in employees if e["id"]==employee_id),None)
     if not emp:
-        raise HTTPException ("Employee not found")
+        raise HTTPException (status_code=404,detail="Employee not found")
 
-    sl=next((s for s in slots if e["id"]==slot_id),None)
-    if not sl:
-        raise HTTPException("Slot not found")
+    room=next((r for r in rooms if r["id"]==room_id),None)
+    if not room:
+        raise HTTPException(status_code=404,detail="Room not found")
     
     for a in assignments:
-        if a["slot_id"]==slot_id:
-            raise HTTPException("Already assigned")
+        if a["room_id"]==room_id:
+            raise HTTPException(status_code=404,detail="Already assigned")
     
-    assignment={"employee_id":employee_id,
-               "slot_id":slot_id}
+    assignment={"employee_id":employee_id, "room_id":room_id}
     assignments.append(assignment)
-    return assignments
+    return assignment
 
 @app.get("/assignments")
 def get_assignments():
