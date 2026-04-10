84d7d6ae08fc20b94a8dc292b50cdee0e5b311da
AshishG-21 <ashish.g_bca24-28@macfast.ac.in>
2026-04-10 15:44:18 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -22,6 +22,30 @@ const addEmployee= async()=>{
   await fetch(API + "/rooms?name=" + roomName,{method:"POST"});
   window.location.reload();
 };
+
+  return (
+     <div>
+      <h2>Employee Room System</h2>
+
+      <h3>Add Employee</h3>
+      <input value={name} onChange={e => setName(e.target.value)}/>
+      <button onClick={addEmployee}>Add</button>
+
+      <h3>Add Room</h3>
+      <input value={roomName} onChange={e => setRoomName(e.target.value)}/>
+      <button onClick={addRoom}>Add</button>
+
+      <h3>Employees</h3>
+      <ul>
+        {employees.map(e =><li key={e.id}>{e.name}</li>)}
+      </ul>
+
+      <h3>Rooms</h3>
+      <ul>
+        {rooms.map(r => <li key={r.id}>{r.room_name}</li>)}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
