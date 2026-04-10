43809f721ee1dd69da18342b17fcf67be0a3efbf
AshishG-21 <ashish.g_bca24-28@macfast.ac.in>
2026-04-10 17:02:27 +0530

    Add files via upload

---
+++ FastAPI-React Project/Frontend/my-react-app/src/App.jsx
@@ -0,0 +1,53 @@
+import React,{ useState,useEffect } from 'react';
+
+function App() {
+  const [name, setName] = useState("");
+  const [roomName, setRoomName] = useState("");
+const [employees, setEmployees] = useState([]);
+const [rooms,setRooms] = useState([]);
+
+const API="http://127.0.0.1:8000";
+
+
+useEffect(()=>{  
+  fetch(API+ "/get_employees").then(res=>res.json()).then(data=> {console.log("Employees:",data);
+    setEmployees(data)});
+  fetch(API + "/get_rooms").then(res=>res.json()).then(data=> {console.log("Rooms:",data);
+     setRooms(data)});
+},[]);
+
+const addEmployee= async()=>{
+  await fetch(API + "/employees?name=" + name,{method:"POST"});
+  window.location.reload();
+};
+  const addRoom=async()=>{
+  await fetch(API + "/rooms?name=" + roomName,{method:"POST"});
+  window.location.reload();
+};
+return (
+     <div>
+      <h2>Employee Room System</h2>
+
+      <h3>Add Employee</h3>
+      <input value={name} onChange={e => setName(e.target.value)}/>
+      <button onClick={addEmployee}>Add</button>
+
+      <h3>Add Room</h3>
+      <input value={roomName} onChange={e => setRoomName(e.target.value)}/>
+      <button onClick={addRoom}>Add</button>
+
+      <h3>Employees</h3>
+      <ul>
+        {employees.map(e =><li key={e.id}>{e.name}</li>)}
+      </ul>
+
+      <h3>Rooms</h3>
+      <ul>
+        {rooms.map(r => <li key={r.id}>{r.room_name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ FastAPI-React Project/Frontend/my-react-app/src/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+employees=[]
+rooms=[]
+assignments=[]
+
+
+@app.post("/employees")
+def add_employees(name:str):
+    emp= {"id":len(employees)+1,"name":name}
+    employees.append(emp)
+    return emp
+
+@app.get("/get_employees")
+def get_employees():
+    return employees
+
+@app.post("/rooms")
+def add_rooms(name:str):
+    room={"id":len(rooms)+1, "room_name":name}
+    rooms.append(room)
+    return room
+
+@app.get("/get_rooms")
+def get_rooms():
+    return rooms
+
+@app.post("/assign")
+def assign(employee_id:int,room_id:int):
+    emp=next((e for e in employees if e["id"]==employee_id),None)
+    if not emp:
+        raise HTTPException (status_code=404,detail="Employee not found")
+
+    room=next((r for r in rooms if r["id"]==room_id),None)
+    if not room:
+        raise HTTPException(status_code=404,detail="Room not found")
+    
+    for a in assignments:
+        if a["room_id"]==room_id:
+            raise HTTPException(status_code=404,detail="Already assigned")
+    
+    assignment={"employee_id":employee_id, "room_id":room_id}
+    assignments.append(assignment)
+    return assignment
+
+@app.get("/assignments")
+def get_assignments():
+    return assignments
+    
+
+
+
+
+
