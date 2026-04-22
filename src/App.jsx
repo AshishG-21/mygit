9388d9d0f6f4ac636aba355ae2201d337a7b2d96
AshishG-21 <ashish.g_bca24-28@macfast.ac.in>
2026-04-22 07:27:04 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+import MainRouter from './components/MainRouter'
+
+function App() {
+  
+
+  return (
+    <>
+      <MainRouter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/Contactus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contactus = () => {
+  return (
+    <div>Contact details: 9738173647</div>
+  )
+}
+
+export default Contactus
